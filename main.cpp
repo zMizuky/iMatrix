c0b7f62c1ef35b9e7310e92a312698c5379ec70b
Ian <48653877+zMizuky@users.noreply.github.com>
2026-01-13 20:50:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
 
 char WINDOW_TITLE[] = "Hello SDL";
 
@@ -7,6 +8,14 @@ int WINDOWS_HEIGHT = 600;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
+
+struct Player {
+    float x = 250;
+    float y = 250;
+};Player player;
+
+const char player_texture_path[] = "./assets/sprites/character.png"; // o spritesheet do player
+SDL_Texture* player_texture;
 
 bool done = false;
 
@@ -34,6 +43,9 @@ int main(){
         SDL_Log("Erro ao criar o renderer: %s", SDL_GetError());
         return -1;
     }
+    
+    player_texture = IMG_LoadTexture(renderer, player_texture_path);
+    SDL_SetTextureScaleMode(player_texture, SDL_SCALEMODE_NEAREST);
 
     while(!done){
         SDL_Event event;
@@ -41,14 +53,36 @@ int main(){
             if(event.type == SDL_EVENT_QUIT){
                 done = true;
             }
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_A) player.x -= 5;
+                if (event.key.key == SDLK_D) player.x += 5;
+                if (event.key.key == SDLK_W) player.y -= 5;
+                if (event.key.key == SDLK_S) player.y += 5;
+            }
 
             SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
             SDL_RenderClear(renderer);
-
+            
+            SDL_FRect player_frame = {
+                0,      //inicio, x
+                0,      //inicio, y
+                16,     //tamanho em x
+                32      //tamanho em y
+            };
+            SDL_FRect player_pos = {
+                player.x,
+                player.y, 
+                16, 
+                32
+            };
+            
+            SDL_RenderTexture(renderer, player_texture, &player_frame, &player_pos);
+            
             SDL_RenderPresent(renderer);
         }
     }
     
+    SDL_DestroyTexture(player_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
